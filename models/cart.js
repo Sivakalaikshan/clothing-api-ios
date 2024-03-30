55cfd0032cc1aa1cb8fd08eae6af890a7da27180
Sivakalaikshan <sivaklaikshan1999@gmail.com>
2024-03-30 19:09:02 +0530

    13 commit

--- models/cart.js
+++ models/cart.js
@@ -2,10 +2,10 @@ const { text } = require('express');
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
+    // userId: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User'
+    // },
     products: [{
         productId: {
             type: mongoose.Schema.Types.ObjectId,
